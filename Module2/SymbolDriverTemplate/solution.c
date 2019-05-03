07acdd254e7d548814a4217b771a48cb3878245e
nikita <nikita1993chizh@gmail.com>
2019-05-04 00:05:24 +0400

    Можно писать в драйвер с нескольких процессов

--- Module2/SymbolDriverTemplate/solution.c
+++ Module2/SymbolDriverTemplate/solution.c
@@ -11,12 +11,11 @@
 MODULE_AUTHOR("Chzh") ;
 MODULE_LICENSE("GPL") ;
 MODULE_DESCRIPTION("This is a very important kernel module");
+// sudo mknod /dev/chrdrv c 700 0 && sudo chmod a+rw /dev/chrdrv
+// ls -la /dev/chrdev
+// sudo chmod a+rw /dev/chrdrv
+// sudo echo "hi" > /dev/chrdrv
 
-static char *kbuf;
-// typedef  unsigned int    __u32;
-// typedef  __u32           __kernel_dev_t;
-// typedef  __kernel_dev_t  dev_t;
-//
 static dev_t first;
 static unsigned int count = 1;
 static int my_major = 700, my_minor = 0;
@@ -28,29 +27,46 @@ static struct cdev *my_cdev;
 
 
 static int mydev_open(struct inode *pinode, struct file *pfile){
-    printk( KERN_INFO "myopen %s\n", DEVICE_NAME);
+    printk( KERN_INFO "OPEN\n");
+    printk( KERN_INFO "myopen %s KBUF_SIZE=%d\n", DEVICE_NAME, KBUF_SIZE);
+    static int counter = 0;
+    char *kbuf = kmalloc(KBUF_SIZE, GFP_KERNEL);
+    pfile -> private_data = kbuf;
+    printk( KERN_INFO "COUNTER=%d\n", counter);
+    printk( KERN_INFO "REFERENCE COUNTER=%d\n", module_refcount(THIS_MODULE));
+
     return 0;
 }
 
 static int mydev_release(struct inode *pinode, struct file *pfile){
     printk( KERN_INFO "mydev_release\n", DEVICE_NAME);
+    char *kbuf = pfile -> private_data;
+    if(kbuf)
+        kfree(kbuf);
+    kbuf= NULL;
+    pfile -> private_data = NULL;
     return 0;
 }
 
 static ssize_t mydev_read(struct file *pfile, char __user *buf, size_t lbuf, loff_t *ppos){
-int nbytes = lbuf - copy_to_user(buf, kbuf + *ppos, lbuf);
-printk( KERN_INFO "READ DEVICE %s nbytes=%d ppos=%d\n", DEVICE_NAME, nbytes, (int)*ppos);
-return nbytes;
+    char *kbuf = pfile -> private_data;
+    static char test_msg[128] = "Hello world\0";
+    memcpy(kbuf, test_msg, 12);
+    int nbytes = lbuf - copy_to_user(buf, kbuf + *ppos, lbuf);
+    printk( KERN_INFO "READ DEVICE %s nbytes=%d ppos=%d\n", DEVICE_NAME, nbytes, (int)*ppos);
+    return nbytes;
 }
 
 static ssize_t mydev_write(struct file *pfile, const char __user *buf, size_t lbuf, loff_t *ppos){
-int nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
-printk( KERN_INFO "WRITE DEVICE %s nbytes=%d ppos=%d\n", DEVICE_NAME, nbytes, (int)*ppos);
-return nbytes;
+    char *kbuf = pfile -> private_data;
+    int nbytes = lbuf - copy_from_user(kbuf + *ppos, buf, lbuf);
+    printk( KERN_INFO "WRITE DEVICE %s nbytes=%d ppos=%d\n", DEVICE_NAME, nbytes, (int)*ppos);
+    return nbytes;
 }
 
 struct file_operations fops = {
         .owner = THIS_MODULE,
+        .open = mydev_open,
         .read = mydev_read,
         .write = mydev_write,
         .release = mydev_release
@@ -58,7 +74,6 @@ struct file_operations fops = {
 
 static int __init hello_init(void) {
     printk( KERN_ALERT "Module started\n" ) ;
-    kbuf = (char*)kmalloc(KBUF_SIZE, GFP_KERNEL);
     //
     first = MKDEV(my_major, my_minor);
     register_chrdev_region(first, count, DEVICE_NAME);
@@ -70,6 +85,9 @@ static int __init hello_init(void) {
 
 static void __exit hello_exit(void) {
     printk( KERN_ALERT "Module unloaded\n" ) ;
+    if(my_cdev)
+        cdev_del(my_cdev);
+    unregister_chrdev_region(first, count);
 }
 
 module_init(hello_init) ;
